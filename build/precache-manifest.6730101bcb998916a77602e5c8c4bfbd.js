a1123191c9a52b1d7cdb20367f12cf9597d010a9
liqiang <a15515358762@qq.com>
2019-10-08 20:25:54 +0800

    打包

---
+++ build/precache-manifest.6730101bcb998916a77602e5c8c4bfbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f033f264bb289086fd655d37256778",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d21160a5f1c655b59e0",
+    "url": "./static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "da2ef47058f7f7fa8587",
+    "url": "./static/css/main.fed22c73.chunk.css"
+  },
+  {
+    "revision": "9d21160a5f1c655b59e0",
+    "url": "./static/js/2.623c8148.chunk.js"
+  },
+  {
+    "revision": "da2ef47058f7f7fa8587",
+    "url": "./static/js/main.eb9d74f3.chunk.js"
+  },
+  {
+    "revision": "0947388cdf64acfc0ceb",
+    "url": "./static/js/runtime-main.0cd8b358.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90721e4bc59d29f41e9d32944133fa23.js"
+  "./precache-manifest.6730101bcb998916a77602e5c8c4bfbd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
